46ee68d27634b1e89d8c7aa7e2491428b17dc7db
Jana Saa <jana.abusaa@gmail.com>
2024-09-23 19:00:05 +0300

    Add Data

---
+++ dbo/Tables/Books.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[Books] (
+    [BookID]        INT           IDENTITY (1, 1) NOT NULL,
+    [Title]         VARCHAR (100) NULL,
+    [Author]        VARCHAR (50)  NULL,
+    [ISBN]          VARCHAR (20)  NULL,
+    [PublishedDate] DATE          NULL,
+    [Genre]         VARCHAR (50)  NULL,
+    [ShelfLocation] VARCHAR (50)  NULL,
+    [CurrentStatus] VARCHAR (50)  NULL,
+    PRIMARY KEY CLUSTERED ([BookID] ASC),
+    CHECK ([CurrentStatus]='Borrowed' OR [CurrentStatus]='Available')
+);
+
